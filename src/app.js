70bc0be95832a99fda94feccfba15361d2926bbc
masaminh <masami.askw@gmail.com>
2020-02-20 16:28:48 +0900

    コメント追加

--- src/app.js
+++ src/app.js
@@ -6,11 +6,14 @@ const Moji = require('@eai/moji')
 
 Moment.tz.setDefault('Asia/Tokyo')
 
+// 最後のa=10が札幌を示している
 const Url = 'https://tv.yahoo.co.jp/search/?q=%E3%82%A4%E3%83%83%E3%83%86Q&a=10'
 const Title = '世界の果てまでイッテQ!'
 
+// スキル起動ハンドラ
 const LaunchRequestHandler = {
   canHandle (handlerInput) {
+    // スキル起動時に反応する
     return handlerInput.requestEnvelope.request.type === 'LaunchRequest'
   },
   async handle (handlerInput) {
@@ -25,10 +28,14 @@ const LaunchRequestHandler = {
       return {
         date: dateString,
         time: timeString,
+        // 番組表の文字列、全角半角の混ざり方が不規則なので、英数は半角、カナは全角に揃える
         title: Moji(titleString).convert('ZEtoHE').convert('HKtoZK').toString()
       }
     }).get().filter(x => x.title.indexOf(Title) === 0)
+    // 特番とかだと違う番組が引っかかることがあるので、番組名先頭に「世界の果てまでイッテQ!」
+    // と書かれているものを対象番組として扱うことにする
 
+    // 今日あるかどうかの判断を行う
     const timestamp = Moment(handlerInput.requestEnvelope.request.timestamp)
     const dateString = timestamp.format('M/D')
     const filtered = programInfos.filter(x => x.date === dateString)
@@ -36,6 +43,7 @@ const LaunchRequestHandler = {
     let speechText
 
     if (filtered.length > 0) {
+      // 今日ある場合は、番組詳細まで返す
       const subStr = filtered.map(x => {
         const timeString = x.time.replace('～', 'から')
         const result = timeString + 'に' + x.title + 'が'
@@ -43,16 +51,19 @@ const LaunchRequestHandler = {
       }).join('、')
       speechText = '今日は' + subStr + 'あります。'
     } else if (programInfos.length > 0) {
+      // 今日はないけど、明日以降見つかったら、日付と時刻を返す。
       const subStr = programInfos.map(x => {
         const result = x.date.replace('/', '月') + '日' + x.time.replace('～', 'から')
         return result
       }).join('と、')
       speechText = '今日はイッテQはありませんが、' + subStr + 'にあるようです。'
     } else {
+      // ない
       speechText = '今日はイッテQはありません。'
     }
 
     return handlerInput.responseBuilder
+      // 最後は全角で返さないとうまく読んでもらえない。
       .speak(Moji(speechText).convert('HEtoZE').toString())
       .getResponse()
   }
