33914cbf19f0be1fb240837cd34ad13d5f6f0ed6
masaminh <masami.askw@gmail.com>
2020-05-05 22:33:56 +0900

    TypeScript化

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,26 @@
 module.exports = {
-    "extends": "standard"
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    "plugin:@typescript-eslint/recommended", 
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint"
+],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+  },
 };
